6b853d53115e966156fc7b71e437b0d0319ffc8f
jww52 <jawszone@gmail.com>
2017-03-03 19:43:03 -0600

    Added columns and example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p className="">Here are few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/location=FortWorth'>Fort Worth, TX</Link>
+        </li>
+        <li>
+          <Link to='/location=HoChiMinh'>Ho Chi Minh City, Vietnam</Link>
+        </li>
+      </ol>
     </div>
   );
 };
